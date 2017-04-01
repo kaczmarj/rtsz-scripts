141267d0794c3f50f893118077840be90fbc64b6
jakubk <jakubk@mit.edu>
2017-04-01 15:49:38 -0400

    enh: add documentation

--- bids_with_ses.py
+++ bids_with_ses.py
@@ -1,30 +1,46 @@
 """Heuristic to convert real-time schizophrenia data to BIDS format.
 
 Adapted from Heudiconv example heuristic:
-https://github.com/nipy/heudiconv/blob/master/heuristics/bids_with_ses.py
-
-BIDS specification: http://bids.neuroimaging.io/bids_spec1.0.1.pdf
+    - https://github.com/nipy/heudiconv/blob/master/heuristics/bids_with_ses.py
+Heudiconv presentation:
+    - http://nipy.org/workshops/2017-03-boston/lectures/bids-heudiconv/
+BIDS specification:
+    - http://bids.neuroimaging.io/bids_spec1.0.1.pdf
 """
 from __future__ import print_function
 import os
 
 
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
-    if template is None or not template:
-        raise ValueError('Template must be a valid format string')
+    if not isinstance(template, str) or not template:
+        raise ValueError('Template must be a non-empty string')
     return template, outtype, annotation_classes
 
 
 def infotodict(seqinfo):
-    """Heuristic evaluator for determining which runs belong where
+    """Heuristic evaluator for determining which runs belong where.
 
-    allowed template fields - follow python string module:
+    Parameters
+    ----------
+    seqinfo : list
+        List of lists, where each sublist is a row of the DICOM information
+        text file.
 
-    item: index within category
-    subject: participant id
-    seqitem: run number during scanning
-    subindex: sub index within group
-    session: scan index for longitudinal acq
+    Returns
+    -------
+    info : dict
+        Key is a tuple of len 3 `(template, output_type, annocation_classes)`,
+        Values can be a list of the DICOM series number(s) or a dict.
+
+    Notes
+    -----
+    Filepath templates can include the following default keys:
+        - item: index within category
+        - subject: participant id
+        - seqitem: run number during scanning
+        - subindex: sub index within group
+        - session: scan index for longitudinal acq
+    Custom keys may also be used.
     """
     # print(seqinfo)
 
@@ -65,7 +81,6 @@ def infotodict(seqinfo):
             elif ('rsfMRI_PA' in protocol_name):
                 info[rest].append({'item': series_number, 'direction': 'PA'})
         # Tasks
-        # QUESTION: are boolean-like strings converted to booleans in seqinfo?
         elif (sl == 32) and (nt == 136) and motion_corrected:
             if ('fMRI_listen' in protocol_name):
                 info[task].append({'item': series_number, 'task_name': 'listen'})
